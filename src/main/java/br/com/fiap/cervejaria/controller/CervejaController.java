d718a60cdf1603926e01dfb778f51c155d307497
Logon Aluno <logonaluno@local.com>
2020-01-23 21:42:35 -0300

    #2

--- src/main/java/br/com/fiap/cervejaria/controller/CervejaController.java
+++ src/main/java/br/com/fiap/cervejaria/controller/CervejaController.java
@@ -1,19 +1,23 @@
 package br.com.fiap.cervejaria.controller;
 
 import br.com.fiap.cervejaria.dto.CervejaDto;
+import br.com.fiap.cervejaria.dto.CreateCervejaDto;
 import br.com.fiap.cervejaria.dto.Tipo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static br.com.fiap.cervejaria.dto.Tipo.ALE;
 import static br.com.fiap.cervejaria.dto.Tipo.PILSEN;
 
 @RestController
+@RequestMapping("cervejas")
 public class CervejaController {
 
     private List<CervejaDto> cervejaDtoList;
@@ -28,7 +32,7 @@ public class CervejaController {
                 BigDecimal.valueOf(12.9),
                 ZonedDateTime.now().minusYears(3)));
 
-        cervejaDtoList.add(new CervejaDto(1,
+        cervejaDtoList.add(new CervejaDto(2,
                 "Marca 2",
                 3.5,
                 ALE,
@@ -37,8 +41,25 @@ public class CervejaController {
     }
 
     @GetMapping
-    public List<CervejaDto> getAll(){
-        return cervejaDtoList;
+    public List<CervejaDto> getAll(@RequestParam(required = false) Tipo tipo){
+        return cervejaDtoList.stream()
+                .filter(it -> tipo == null || it.getTipo().equals(tipo))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("{id}")
+    public CervejaDto findById(@PathVariable Integer id){
+        return cervejaDtoList.stream()
+                .filter(it -> it.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public CervejaDto create(@RequestBody CreateCervejaDto createCervejaDto){
+        CervejaDto cervejaDto = new CervejaDto(createCervejaDto, cervejaDtoList.size() + 1);
+        cervejaDtoList.add(cervejaDto);
+        return cervejaDto;
     }
 
     @GetMapping("/hello")
--- src/main/java/br/com/fiap/cervejaria/dto/CreateCervejaDto.java
+++ src/main/java/br/com/fiap/cervejaria/dto/CreateCervejaDto.java
@@ -3,42 +3,12 @@ package br.com.fiap.cervejaria.dto;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 
-public class CervejaDto {
-
-    private Integer id;
+public class CreateCervejaDto {
     private String marca;
     private Double teorAlcoolico;
     private Tipo tipo;
     private BigDecimal preco;
     private ZonedDateTime dataLancamento;
-
-    public CervejaDto() {}
-
-    public CervejaDto(Integer id, String marca, Double teorAlcoolico, Tipo tipo, BigDecimal preco, ZonedDateTime dataLancamento) {
-        this.id = id;
-        this.marca = marca;
-        this.teorAlcoolico = teorAlcoolico;
-        this.tipo = tipo;
-        this.preco = preco;
-        this.dataLancamento = dataLancamento;
-    }
-
-    public CervejaDto(CreateCervejaDto createCervejaDto, Integer id) {
-        this.id = id;
-        this.marca = createCervejaDto.getMarca();
-        this.teorAlcoolico = createCervejaDto.getTeorAlcoolico();
-        this.tipo = createCervejaDto.getTipo();
-        this.preco = createCervejaDto.getPreco();
-        this.dataLancamento = createCervejaDto.getDataLancamento();
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
     public String getMarca() {
         return marca;
